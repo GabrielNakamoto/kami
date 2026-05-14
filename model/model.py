80daf2f20c88dbf21fc13353aeb410f772599c93
Gabriel Nakamoto <gabriel@nakamoto.ca>
2026-05-14 11:17:45 -0400

    added lc0 attention, modified input embedding/projection

--- model/model.py
+++ model/model.py
@@ -1,64 +1,61 @@
 from tinygrad.tensor import Tensor
 from tinygrad.nn import Embedding, Linear, RMSNorm
+from .lcattn import LeelaAttention
 
 class TransformerBlock:
-    def __init__(self, dim, n_heads):
+    def __init__(self, dim, n_heads, use_lc_attn:bool=False):
+        self.lcattn = LeelaAttention(dim, n_heads) if use_lc_attn else None
         self.dim = dim
-        self.qkv_proj = Linear(dim, dim*3)
-        self.attn_proj = Linear(dim, dim)
+        self.qkv_proj = None if use_lc_attn else Linear(dim, dim*3, bias=False)
+        self.attn_proj = None if use_lc_attn else Linear(dim, dim, bias=False)
         self.n_heads = n_heads
         self.head_dim = dim // n_heads
         self.ffn = [
-            Linear(dim, dim*4),
+            Linear(dim, dim*2),
             Tensor.silu,
-            Linear(dim*4, dim)
+            Linear(dim*2, dim)
         ]
         self.attn_norm = RMSNorm(dim)
         self.ffn_norm = RMSNorm(dim)
-    def _attention(self, x: Tensor) -> Tensor: # x(bchsz, 64, dim)
+    def _attention(self, x: Tensor, dropout_p:float=0.0) -> Tensor: # x(bchsz, 64, dim)
         bchsz, seqln = x.shape[0], x.shape[1]
         xqkv = self.qkv_proj(x).reshape((bchsz, seqln, 3, self.n_heads, self.head_dim))
         getf = lambda n: xqkv[:,:,n].transpose(1,2)
         q, k, v = getf(0), getf(1), getf(2)
-        attn = q.scaled_dot_product_attention(k, v, is_causal=False).transpose(2,1).reshape((bchsz, seqln, self.dim))
+        attn = q.scaled_dot_product_attention(k, v, is_causal=False, dropout_p=dropout_p).transpose(2,1).reshape((bchsz, seqln, self.dim))
+
         return self.attn_proj(attn)
     def __call__(self, x: Tensor) -> Tensor:
-        x = x + self._attention(self.attn_norm(x)).dropout(0.05)
+        if self.lcattn: x = x + self.lcattn(self.attn_norm(x))
+        else: x = x + self._attention(self.attn_norm(x), dropout_p=0.05)
         return x + self.ffn_norm(x).sequential(self.ffn).dropout(0.05)
 
 class Model:
-    def __init__(self, dim: int, layers: int, n_global, n_heads: int):
+    def __init__(self, dim: int, layers: int, n_global, n_heads: int, use_lc_attn:bool=False):
         self.dim = dim
+        self.proj_in = Linear(15, dim)
         self.pos_emb = Embedding(64, dim)
-        self.piece_emb = Embedding(13, dim)
-        self.ply_emb = Embedding(8, dim)
-        self.ply_project = Linear(dim*8, dim)
-        self.global_proj = [
-            Linear(n_global, dim),
-            Tensor.silu,
-            Linear(dim, dim)
-        ]
         self.final_norm = RMSNorm(dim)
-        self.blocks = [TransformerBlock(dim, n_heads) for _ in range(layers)]
+        self.blocks = [TransformerBlock(dim, n_heads, use_lc_attn=use_lc_attn) for _ in range(layers)]
         self.policy_head = Linear(dim, 73)
 
-        self.value_proj = Linear(dim, 1)
+        self.value_proj = [
+            Linear(dim, 128),
+            Linear(128, 1)
+        ]
 
     def __call__(self, pieces: Tensor, global_features: Tensor) -> tuple[Tensor, Tensor]:
         B = pieces.shape[0]
-        pieces = self.piece_emb(pieces.reshape(B,8,64))                     # (B,8,64,dim)
-        pieces = pieces + self.ply_emb(Tensor.arange(8)).reshape(1,8,1,-1)  # (B,8,64,dim)
-        pieces = pieces.permute(0,2,1,3).reshape(B,64,8*self.dim)           # (B,64,8*dim)
-        pieces = self.ply_project(pieces)                                   # (B,64,dim)
-        pieces = pieces + self.pos_emb(Tensor.arange(64))
-        global_features = global_features.sequential(self.global_proj).unsqueeze(1)
-        x = global_features.cat(pieces, dim=1) # x(batch, 65, dim)
+        pt = pieces.reshape(B, 64, 8)
+        g = global_features.unsqueeze(1).expand((B,64,-1))
+        x = pt.cat(g, dim=-1)
+        x = self.proj_in(x)
+        x = x + self.pos_emb(Tensor.arange(64))
         x = x.sequential(self.blocks)
         x = self.final_norm(x)
-        squares = x[:,1:,:]
-        global_tok = x[:,0,:]
-
-        p = self.policy_head(squares).reshape(-1, 4672)
-        v = self.value_proj(global_tok).tanh().squeeze(-1)
+        # include square info for value head
+        vx = x.mean(axis=1)
+        p = self.policy_head(x).reshape(-1, 4672)
+        v = vx.sequential(self.value_proj).tanh().squeeze(-1)
         return p, v
 
--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from tinygrad.tensor import Tensor
 from tinygrad.engine.jit import TinyJit
-import numpy as np, wandb
+import numpy as np, wandb, os
 from model import Model
 from tinygrad.nn.optim import AdamW
 from tinygrad.nn.state import get_parameters, safe_save, get_state_dict
@@ -9,42 +9,40 @@ from tinygrad.device import Device
 
 print(Device.DEFAULT)
 
-N = 10000000
+N = 100000
 X_pieces = np.lib.format.open_memmap("pieces.np", shape=(N,512))
 X_games = np.lib.format.open_memmap("globals.np", shape=(N,7))
 Y_move = np.lib.format.open_memmap("moves.np", shape=(N,4672))
 Y_outcome = np.lib.format.open_memmap("outcomes.np", shape=(N,))
 
-d_model = 256
-depth = 10
-heads = 8
+d_model = 128
+depth = 6
+heads = 4
 c_value = 0.1
-batch_size = 1024
+batch_size = 512
 warmup_steps = 2e3
 peak_lr = 4e-4
 steps = 100000
 
-model = Model(d_model, depth, 7, heads)
+model = Model(d_model, depth, 7, heads, True)
 optim = AdamW(get_parameters(model), lr=3e-4, weight_decay=0.01)
 
 n_params = sum(map(Tensor.numel, get_state_dict(model).values()))
 
-run = wandb.init(
-    entity="raine1-me",
-    project="chessformer",
-    config={
-        "d_model" : d_model,
-        "depth" : depth,
-        "heads" : heads,
-        "n_params" : n_params,
-        "c_value" : c_value,
-        "batch_size" : batch_size,
-        "peak_lr" : peak_lr,
-        "training_steps" : steps
-    }
-)
+config={
+    "d_model" : d_model,
+    "depth" : depth,
+    "heads" : heads,
+    "n_params" : n_params,
+    "c_value" : c_value,
+    "batch_size" : batch_size,
+    "peak_lr" : peak_lr,
+    "training_steps" : steps
+}
 
-valid_N = 5000
+run = wandb.init(entity="raine1-me", project="chessformer", config=config) if os.getenv('WANDB', False) else None
+
+valid_N = 500
 train_N = N - valid_N
 
 def random_batch(n):
@@ -89,8 +87,8 @@ for t in range(steps):
         preds = model(eval_xp, eval_xg)[0].masked_fill(eval_yp < 0, -1e9).argmax(axis=-1)
         targets = eval_yp.maximum(0).argmax(axis=-1)
         acc = (preds == targets).float().mean().item()
-        # print(f"step: {t}, loss={loss.item():.2f}, acc={acc*100.:.2f}%")
-        run.log({"acc":acc*100, "loss":loss.item(), "policy_loss" : policy_loss.item(), "value_loss" : value_loss.item()})
+        if run: run.log({"acc":acc*100, "loss":loss.item(), "policy_loss" : policy_loss.item(), "value_loss" : value_loss.item()})
+        else: print(f"step: {t}, loss={loss.item():.2f}, acc={acc*100.:.2f}%")
     if t % 1000 == 0:
         Tensor.training = False
-        safe_save(get_state_dict(model), "model.safetensors", metadata=run.config.as_dict())
+        safe_save(get_state_dict(model), "model.safetensors", metadata=config)
