4122a5d109bac30ee5e77e369b6a414388f22cd5
Gabriel Nakamoto <gabriel@nakamoto.ca>
2026-05-22 16:10:46 -0400

    first try mcts

--- model/__init__.py
+++ model/__init__.py
@@ -1,2 +1,1 @@
 from .model import Model
-from .search import search
--- model/search.py
+++ model/search.py
@@ -1,66 +1,67 @@
-from __future__ import annotations
-import copy
+from tinygrad.tensor import Tensor
 import numpy as np
+import math
+from util.convert import board_to_tensor, get_global_features, build_move_mapping
+from model import Model
 import chess
 
-def search():
-    pass
+# We rollout/explore simulations to build a tree from current game state.
+# Nodes are states and directed edges are legal actions
+# Q = expected reward for taking edge
+# N = # of times edge/node has been visited
+# P = policy prior, distribution over actions from a given state
+# L = legal move indices
+# For each (s,a) dict represent the enumeration over actions as a np array for efficient puct computation
+move_map = build_move_mapping()
+inv_move_map = { v:k for k, v in move_map.items() }
 
-"""
-Ns = # times state visited, cache for upper bound calculation
-Nsa = # times edge visited
-priors = model prediction of taking an edge
-Qsa = expected reward/win rate of taking an edge based on search frequency and model value head
+class MCTS:
+    def __init__(self, model:Model, c_puct:float=1.0):
+        self.model = model
+        self.c_puct = c_puct
+        self.Qsa, self.Nsa, self.Ps, self.Ls, self.Es = {}, {}, {}, {}, {}
 
-u = upper confidence bound, used to decide where to explore next
-    - depends on Qsa (expected) + c_pcut (exploration constant) * policy head output 
-        and exploration weighted on how many times been visited relative to parent state of edge
+    def __call__(self, fen:str, num_sims:int=5):
+        self.Qsa, self.Nsa, self.Ps, self.Ls, self.Es = {}, {}, {}, {}, {}
+        for _ in range(num_sims): self.sim(fen)
+        s = chess.Board(fen)._transposition_key()
+        a = self.Nsa[s].argmax(-1)
+        return chess.Move(*inv_move_map[self.Ls[s][a]])
 
-A inference time pick edge from current state with greatest visits?
+    # https://suragnair.github.io/posts/alphazero.html
+    def sim(self, fen:str):
+        board = chess.Board(fen)
+        s = board._transposition_key()
 
-best_action = max(Nsa[s].values())
-"""
+        if s in self.Es:
+            return -self.Es[s]
 
-"""
+        if board.is_game_over(): 
+            r, z = board.result(), 0.0
+            if r == "1-0": z = 1.0
+            elif r == "0-1": z = -1.0
+            if board.turn != chess.WHITE: z = -z
+            self.Es[s] = z
+            return -z
 
+        if s not in self.Ps:
+            pl, vl = self.model(
+                Tensor(board_to_tensor(board, not board.turn)),
+                Tensor(get_global_features(board, board.turn))
+            )
+            indices = [move_map[(lm.from_square, lm.to_square, lm.promotion)] for lm in board.generate_legal_moves()]
+            self.Ls[s]=indices
+            self.Ps[s]=pl.flatten()[indices].softmax().numpy()
+            self.Qsa[s]=np.zeros(len(indices), dtype=np.float32)
+            self.Nsa[s]=np.zeros(len(indices), dtype=np.uint32)
+            return -vl.dot([1.0, 0.0, -1.0]).item()
 
-Nsa, Qsa, priors, Ns = {}, {}, {}, {}
+        best = (self.Qsa[s] + self.c_puct * self.Ps[s] * math.sqrt(max(self.Nsa[s].sum(), 1)) / (1. + self.Nsa[s])).argmax(-1)
+        board.push(chess.Move(*inv_move_map[self.Ls[s][best]]))
 
-def search(state:GameState, model:Model, player: chess.Color, cpuct:float=0.1):
-    s = str(state)
-    if state.board.is_game_over():
-        if outcome := state.board.outcome():
-            winner = outcome.winner
-            if not winner: return 1e-6
-            elif winner == player: return 1.0
-            else: return -1.0
+        next_fen = board.fen()
+        v = self.sim(next_fen)
 
-    if s not in priors:
-        priors[s], v = model(state.piece_tensor(), state.global_tensor())
-        Ns[s] = 0
+        self.Qsa[s][best] = (self.Nsa[s][best]*self.Qsa[s][best] + v) / (self.Nsa[s][best] + 1)
+        self.Nsa[s][best] += 1
         return -v
-
-    # Pick best upper bound confidence
-    best = (-float("inf"), -1)
-    valids = list(state.board.legal_moves)
-    for a in range(len(valids)):
-        if (s, a) in Qsa:
-            # doesnt work right now, priors[s][a], policy output Tensor cant be indexed like that
-            u = Qsa[(s,a)] + cpuct * priors[s][a] * np.sqrt(Ns[s]) / (1 + Nsa[s][a])
-        else:
-            u = cpuct * priors[s][a] * np.sqrt(Ns[s] + 1e-8)
-        if u > best[0]: best = (u,a)
-
-    a = best[1]
-    next_s = copy.copy(state).push(valids[a])
-    v = search(next_s, model, player, cpuct=cpuct)
-    
-    if (s, a) in Qsa:
-        Qsa[(s,a)]=(Nsa[(s,a)] * Qsa[(s,a) + v] / (Nsa[(s,a)] + 1))
-        Nsa[s][a]+=1
-    else:
-        Qsa[(s,a)]=v
-        Nsa[s][a]=1
-    Ns[s] += 1
-    return -v
-"""
